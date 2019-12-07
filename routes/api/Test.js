517df818a7578f0037c74f4e8b29da47d775c3f5
nouranayad98 <nouran.ayad@student.guc.edu.eg>
2019-12-07 10:47:39 +0200

    filterGender

--- routes/api/Test.js
+++ routes/api/Test.js
@@ -1,60 +1,124 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios')
-router.get('/', async (req, response) => {
-    const sitemap = await axios.post(
-      'https://api.webscraper.io/api/v1/sitemap?api_token=FcHU0rvsmldc8joVoslI6cbXZdU5j4sVNWIxyRiJwVmv5GR2WaSQSp8rooN9',
-      {"_id":"tedbaker","startUrl":["https://www.tedbaker.com"],"selectors":[{"id":"categs","type":"SelectorLink","parentSelectors":["_root"],"selector":".js-root-category a.categories_main","multiple":true,"delay":0},{"id":"subcategs","type":"SelectorLink","parentSelectors":["categs"],"selector":".cat-pnl-swiper a","multiple":true,"delay":0},{"id":"name","type":"SelectorText","parentSelectors":["subcategs"],"selector":"p.summary","multiple":true,"regex":"","delay":0},{"id":"price","type":"SelectorText","parentSelectors":["subcategs"],"selector":"div:nth-of-type(n+37) li.price","multiple":true,"regex":"","delay":0},{"id":"image","type":"SelectorImage","parentSelectors":["subcategs"],"selector":"div:nth-of-type(n+49) img","multiple":true,"delay":0}]}
-      
+var geolocation = require('geolocation')
+router.get('/screate', async (req, response) => {
+  const sitemap = await axios.post(
+    'https://api.webscraper.io/api/v1/sitemap?api_token=rozN4A44lCBCkcBLLP2C8vCrZs7An7EZsrrOS9eGDrUCAToBRxffs8zab2nS',
+    {
+      _id: 'bershka8',
+      startUrl: ['https://www.bershka.com/eg/'],
+      selectors: [
+        {
+          id: 'mainCategory',
+          type: 'SelectorLink',
+          parentSelectors: ['_root'],
+          selector: '.hide-mobile .main-menu-element a',
+          multiple: true,
+          delay: 0
+        },
+        {
+          id: 'item',
+          type: 'SelectorText',
+          parentSelectors: ['mainCategory'],
+          selector:
+            'grid-product:nth-of-type(n+16) .product-with-colors a.linkTags',
+          multiple: true,
+          regex: '',
+          delay: 0
+        },
+        {
+          id: 'picture',
+          type: 'SelectorImage',
+          parentSelectors: ['mainCategory'],
+          selector: 'grid-product:nth-of-type(n+54) a img.principalImg',
+          multiple: true,
+          delay: 0
+        }
+      ]
+    }
+  )
+  const job = await axios.post(
+    'https://api.webscraper.io/api/v1/scraping-job?api_token=rozN4A44lCBCkcBLLP2C8vCrZs7An7EZsrrOS9eGDrUCAToBRxffs8zab2nS',
+    {
+      sitemap_id: sitemap.data.data.id,
+      driver: 'fast', // "fast" or "fulljs"
+      page_load_delay: 2000,
+      request_interval: 2000,
+      proxy: 0 // optional. 0 - no proxy, 1 - use proxy. Or proxy id for Scale plan users
+    }
+  )
+
+  console.log(job.data)
+  response.json(job.data.data)
+})
+
+router.get('/getdata', async (req, response) => {
+  const jobs = await axios.get(
+    'https://api.webscraper.io/api/v1/scraping-jobs?api_token=rozN4A44lCBCkcBLLP2C8vCrZs7An7EZsrrOS9eGDrUCAToBRxffs8zab2nS'
+  )
+  var result = []
+  // console.log(jobs.data.data)
+  for (var i = 0; i < jobs.data.data.length; i++) {
+    console.log(jobs.data.data[i].id)
+    const sData = await axios.get(
+      'https://api.webscraper.io/api/v1/scraping-job/' +
+        jobs.data.data[i].id +
+        '/json?api_token=rozN4A44lCBCkcBLLP2C8vCrZs7An7EZsrrOS9eGDrUCAToBRxffs8zab2nS'
     )
-    const job = await axios.post(
-              'https://api.webscraper.io/api/v1/scraping-job?api_token=FcHU0rvsmldc8joVoslI6cbXZdU5j4sVNWIxyRiJwVmv5GR2WaSQSp8rooN9',
-              {
-                "sitemap_id": sitemap.data.data.id,
-                "driver": "fast", // "fast" or "fulljs"
-                "page_load_delay": 2000,
-                "request_interval": 2000,
-                "proxy": 0 // optional. 0 - no proxy, 1 - use proxy. Or proxy id for Scale plan users
-              }
-            )
+    result.push(sData.data)
+  }
 
-          
-      const data= await axios.get(
-                'https://api.webscraper.io/api/v1/scraping-job/'+job.data.data.id+'/json?api_token=FcHU0rvsmldc8joVoslI6cbXZdU5j4sVNWIxyRiJwVmv5GR2WaSQSp8rooN9'
-               )
+  var finalArray = []
+  for (var j = 0; j < result.length; j++) {
+    var array = result[j].split('\n')
+    for (var i = 0; i < array.length - 1; i++) {
+      finalArray.push(JSON.parse(array[i]))
+    }
+  }
+  console.log(result.length)
+  response.json(finalArray)
+})
 
-              console.log(data.data)
-               response.json(data.data)
+router.post('/filterGender', async (req, response) => {
+  const gender = req.body.gender
 
+  const data = await getData()
+  var result = []
+  for (var i = 0; i < data.length; i++) {
+    var g = data[i].main_category
+    if (g == gender) {
+      result.push(data[i])
+    }
+  }
+  // console.log(gender)
+  response.json(result)
+})
+async function getData() {
+  const jobs = await axios.get(
+    'https://api.webscraper.io/api/v1/scraping-jobs?api_token=rozN4A44lCBCkcBLLP2C8vCrZs7An7EZsrrOS9eGDrUCAToBRxffs8zab2nS'
+  )
+  var result = []
+  // console.log(jobs.data.data)
+  for (var i = 0; i < jobs.data.data.length; i++) {
+    console.log(jobs.data.data[i].id)
+    const sData = await axios.get(
+      'https://api.webscraper.io/api/v1/scraping-job/' +
+        jobs.data.data[0].id +
+        '/json?api_token=rozN4A44lCBCkcBLLP2C8vCrZs7An7EZsrrOS9eGDrUCAToBRxffs8zab2nS'
+    )
+    result.push(sData.data)
+  }
 
-  })
-    module.exports = router
+  var finalArray = []
+  for (var j = 0; j < result.length; j++) {
+    var array = result[j].split('\n')
+    for (var i = 0; i < array.length - 1; i++) {
+      finalArray.push(JSON.parse(array[i]))
+    }
+  }
+  // console.log(finalArray)
+  return finalArray
+}
 
-//     .then (res => {
-//          axios.post(
-//         'https://api.webscraper.io/api/v1/scraping-job?api_token=FcHU0rvsmldc8joVoslI6cbXZdU5j4sVNWIxyRiJwVmv5GR2WaSQSp8rooN9',
-//         {
-//           "sitemap_id": res.data.id,
-//           "driver": "fast", // "fast" or "fulljs"
-//           "page_load_delay": 2000,
-//           "request_interval": 2000,
-//           "proxy": 0 // optional. 0 - no proxy, 1 - use proxy. Or proxy id for Scale plan users
-//         }
-//       ).then(res =>{
-//          const data= axios.get(
-//           'https://api.webscraper.io/api/v1/scraping-job/'+res.data.id+'/json?api_token=FcHU0rvsmldc8joVoslI6cbXZdU5j4sVNWIxyRiJwVmv5GR2WaSQSp8rooN9'
-
-//         )
-
-//       } )
-//     }
-//     )
-//     response.json({data:sitemap})
-// })
-// // router.get('/', async (req, res) => {
-// //   res.json('hii')
-// // })
-
-
-
-// // module.exports = router
+module.exports = router
