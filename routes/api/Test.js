a8069d8b13b21bae7412d5723e2ce22777eade14
SeifAH <seif.ahmedh.s5@gmail.com>
2019-12-09 14:08:18 +0200

    asd

--- routes/api/Test.js
+++ routes/api/Test.js
@@ -93,25 +93,24 @@ router.get('/screatelcwakiki', async (req, response) => {
 })
 
 
-// router.get('/screatepullansbear', async (req, response) => {
-//   const sitemap = await axios.post(
-//       'https://api.webscraper.io/api/v1/sitemap?api_token=rozN4A44lCBCkcBLLP2C8vCrZs7An7EZsrrOS9eGDrUCAToBRxffs8zab2nS',
-//       {"_id":"pullandbear","startUrl":["https://www.pullandbear.com/eg/"],"selectors":[{"id":"main_cat","type":"SelectorLink","parentSelectors":["_root"],"selector":".title a","multiple":true,"delay":0},{"id":"sub_cat","type":"SelectorLink","parentSelectors":["main_cat"],"selector":"li.level-2:nth-of-type(n+2) > a","multiple":true,"delay":0},{"id":"product_name","type":"SelectorText","parentSelectors":["sub_cat"],"selector":"div.name","multiple":true,"regex":"","delay":0},{"id":"product_img","type":"SelectorImage","parentSelectors":["sub_cat"],"selector":"div:nth-of-type(n+9) img[data-object-fit]","multiple":true,"delay":0},{"id":"product_price","type":"SelectorText","parentSelectors":["sub_cat"],"selector":"div:nth-of-type(n+47) [data-instance-id] .price span","multiple":true,"regex":"","delay":0}]}
-//   )
-//   const job = await axios.post(
-//     'https://api.webscraper.io/api/v1/scraping-job?api_token=rozN4A44lCBCkcBLLP2C8vCrZs7An7EZsrrOS9eGDrUCAToBRxffs8zab2nS',
-//     {
-//       sitemap_id: sitemap.data.data.id,
-//       driver: 'fast', // "fast" or "fulljs"
-//       page_load_delay: 2000,
-//       request_interval: 2000,
-//       proxy: 0 // optional. 0 - no proxy, 1 - use proxy. Or proxy id for Scale plan users
-//     }
-//   )
+router.get('/screatetest', async (req, response) => {
+  const sitemap = await axios.post(
+      'https://api.webscraper.io/api/v1/sitemap?api_token= I56Cl9fdXBgKuPqN4RD6aoS4qITCOMIpuf9J7Nlc2WK06uf3bxKU4pZ1oAKy',
+      {"_id":"lcc2","startUrl":["https://www.lcwaikiki.eg/en/c/women/?page=[2-30]"],"selectors":[{"id":"price","type":"SelectorText","parentSelectors":["_root"],"selector":"div:nth-of-type(n+10) span","multiple":true,"regex":"","delay":0},{"id":"img","type":"SelectorImage","parentSelectors":["_root"],"selector":"img.product-item-image","multiple":true,"delay":0},{"id":"product_name","type":"SelectorText","parentSelectors":["_root"],"selector":"div:nth-of-type(n+7) p","multiple":true,"regex":"","delay":0}]}  )
+  const job = await axios.post(
+    'https://api.webscraper.io/api/v1/scraping-job?api_token= I56Cl9fdXBgKuPqN4RD6aoS4qITCOMIpuf9J7Nlc2WK06uf3bxKU4pZ1oAKy',
+    {
+      sitemap_id: sitemap.data.data.id,
+      driver: 'fast', // "fast" or "fulljs"
+      page_load_delay: 2000,
+      request_interval: 2000,
+      proxy: 0 // optional. 0 - no proxy, 1 - use proxy. Or proxy id for Scale plan users
+    }
+  )
 
-//   console.log(job.data)
-//   response.json(job.data)
-// })
+  console.log(job.data)
+  response.json(job.data)
+})
 
 
 
@@ -136,22 +135,22 @@ router.get('/screateamericaneagle', async (req, response) => {
 })
 
 
-router.get('/getdata', async (req, response) => {
-  const jobs = await axios.get(
-    'https://api.webscraper.io/api/v1/scraping-jobs?api_token=rozN4A44lCBCkcBLLP2C8vCrZs7An7EZsrrOS9eGDrUCAToBRxffs8zab2nS'
-  )
-  var result = []
-  // console.log(jobs.data.data)
-  for (var i = 0; i < jobs.data.data.length; i++) {
-    console.log(jobs.data.data[i].id)
-    const sData = await axios.get(
-      'https://api.webscraper.io/api/v1/scraping-job/' +
-        jobs.data.data[i].id +
-        '/json?api_token=rozN4A44lCBCkcBLLP2C8vCrZs7An7EZsrrOS9eGDrUCAToBRxffs8zab2nS'
-    )
-    result.push(sData.data)
-  }
-  // console.log(result)
-  response.json(result)
-})
+// router.get('/getdata', async (req, response) => {
+//   const jobs = await axios.get(
+//     'https://api.webscraper.io/api/v1/scraping-jobs?api_token=rozN4A44lCBCkcBLLP2C8vCrZs7An7EZsrrOS9eGDrUCAToBRxffs8zab2nS'
+//   )
+//   var result = []
+//   // console.log(jobs.data.data)
+//   for (var i = 0; i < jobs.data.data.length; i++) {
+//     console.log(jobs.data.data[i].id)
+//     const sData = await axios.get(
+//       'https://api.webscraper.io/api/v1/scraping-job/' +
+//         jobs.data.data[i].id +
+//         '/json?api_token=rozN4A44lCBCkcBLLP2C8vCrZs7An7EZsrrOS9eGDrUCAToBRxffs8zab2nS'
+//     )
+//     result.push(sData.data)
+//   }
+//   // console.log(result)
+//   response.json(result)
+// })
 module.exports = router
